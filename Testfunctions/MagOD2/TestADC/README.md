06f8dbea28f00949f7ba76447edc8a3fed152a82
Leon Abelmann <l.abelmann@utwente.nl>
2020-04-14 16:43:21 +0200

    update with interupt based readout

--- Testfunctions/MagOD2/TestADC/README.md
+++ Testfunctions/MagOD2/TestADC/README.md
@@ -1,5 +1,16 @@
 # TestADC.ino
-Leon Abelmann, 7 April 2019
-Program to test the ADC1115 converters on the MagOD2 board.
+Leon Abelmann, April 2020
+
+Program to test the ADS1115 converters on the MagOD2 board.
 Will read the ADC values, send them to the serial port and display
-them on the TFTM50 screen.
+them on the TFTM50 screen. Calculates the standard deviation of N
+measurements, and max-min values (only serial output).
+
+Requires MagOD_ADS115, which is a modification of the Adafruit ADS1015
+library to incorporate interupt based readout. You can set the
+samplingrate (sampleRate).
+
+ADC0 is read using interupt on port 13 of ESP32 (connected to
+ALERT/RDY pin of ADS1115)
+
+ADC1 is read using a suitable delay.
--- Testfunctions/MagOD2/TestADC/TestADC.ino
+++ Testfunctions/MagOD2/TestADC/TestADC.ino
@@ -1,14 +1,15 @@
 #include <SPI.h> // Communication
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <Adafruit_RA8875.h> // TFTM50 screen
-#include <Adafruit_ADS1015.h>  // ADC library
+#include "src/MagOD_ADS1115/MagOD_ADS1115.h" // Modified Adafruit_ADS1015 lib
 
-#define RA8875_INT 16
-#define RA8875_CS 5 
-#define RA8875_RESET 17
+const int N          = 10; // Averaging
+uint8_t   sampleRate = 4;  /* Sample rates: 0: 8SPS, 1: 16 SPS, 2: 32
+			   SPS, 3: 64 SPS, 4: 128 SPS, 5: 250 SPS 6:
+			   475 SPS 7: 860 SPS */
+int channel[] = {0,1,2,3}; // Which channels to sample (only ads0) {0,1,2,3} 
+int numChan   = 4;         // Number of channels in above channel array (max 4)
 
-/* Intialize screen */
-Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS,RA8875_RESET);
 /* Initialize adc */
 /* Adafruit_ADS1015(uint8_t i2cAddress = ADS1015_ADDRESS);
    0x48 (1001000) ADR -> GND
@@ -20,8 +21,38 @@ Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS,RA8875_RESET);
    U5 with address pin to VDD (3.3V), used for AIN4-7 */
 #define ADS1015_ADDRESS_0 (0x48)// 1001 000 (ADDR -> GND)
 #define ADS1015_ADDRESS_1 (0x49)// 1001 001 (ADDR -> VDD)
+
+// ADS1115 for adc0-3 (0x48) has ALERT/RDY connected to pin 13 of EPS32  
+#define ADS1115_RDY 13
+
 Adafruit_ADS1115 ads0(ADS1015_ADDRESS_0); //adc0-3;
 Adafruit_ADS1115 ads1(ADS1015_ADDRESS_1); //adc4-7;
+
+/* Intialize screen */
+#define RA8875_INT 16
+#define RA8875_CS 5 
+#define RA8875_RESET 17
+Adafruit_RA8875 tft = Adafruit_RA8875(RA8875_CS,RA8875_RESET);
+
+/* program variables, no immediate need to adjust from here */
+struct adc {
+  double average; 
+  double stdev; 
+  double extremes; 
+};
+
+bool adcReady = false; // Flag that raises when conversion is done
+int iChan     = 0;     // Counter for channel array [0..chanNum].
+uint16_t adc0[N], adc1[N], adc2[N], adc3[N]; //Bit values of adc0
+uint16_t adc4[N], adc5[N], adc6[N], adc7[N]; //Bit values of adc1
+adc adc0r, adc1r, adc2r, adc3r, adc4r, adc5r, adc6r, adc7r; // results
+
+uint16_t adcValue; 
+long int currenttime;
+long int sampletime;
+char string[15]; //Buffer holding string for dtostrf
+int samplenum = 0;
+int i = 0;
 
 /* Coil variables (to switch them off) */
 uint8_t Coil_x = 33; // output of the coils in the x direction
@@ -31,6 +62,56 @@ const int ledChannel_x = 3; /*0-15*/
 const int ledChannel_y = 4; /*0-15*/
 const int ledChannel_z = 5; /*0-15*/
 
+adc analyze(uint16_t adcin[], int N) {
+  double sum     = 0;
+  double average = 0;
+  double sumsqr  = 0;
+  double diff    = 0;
+  double min     = adcin[0];
+  double max     = adcin[0];
+  adc results;
+  // First calculate average value
+  for  (int i = 0; i < N; i++) {
+    sum = sum + adcin[i];
+  }
+  average = sum/N;
+  /* Than calculate sum of square of difference with average, and
+     while you at it, calculate min and max values */
+  for  (int i = 0; i < N; i++) {
+    diff = (adcin[i]-average);
+    sumsqr = sumsqr + diff*diff;
+    if ( adcin[i] < min) {min = adcin[i];};
+    if ( adcin[i] > max) {max = adcin[i];};
+  }
+  // Return results
+  results.average = average;
+  if (N>1) {
+    results.stdev = sqrt((1.0/(N-1))*sumsqr);
+  }
+  else {
+    results.stdev =0;
+  }
+  results.extremes= (max-min);
+  return results;
+}
+
+void tftWrite(int i,adc adcr){
+    tft.textSetCursor(1, 20+i*15);
+    tft.textWrite("ADC");
+    tft.textSetCursor(24, 20+i*15);
+    tft.textWrite(dtostrf(i,1,0,string),2);
+    tft.textSetCursor(40, 20+i*15);
+    tft.textWrite(dtostrf(adcr.average,5,0,string),6);
+    tft.textSetCursor(100, 20+i*15);
+    tft.textWrite(dtostrf(adcr.stdev,5,0,string),6);
+}
+
+void serialWrite(int i,adc adcr){
+  Serial.print("adc");Serial.print(i);Serial.print(": ");
+  Serial.print(adcr.average);Serial.print(", ");
+  Serial.print(adcr.stdev);Serial.print(", ");
+  Serial.println(adcr.extremes);
+}
 
 void setup () {
   Serial.begin(115200);
@@ -62,6 +143,17 @@ void setup () {
   ads0.begin();
   ads1.setGain(GAIN_ONE);
   ads1.begin();
+  // ads0 is interupt based. Pin 13 is connect to ALERT/RDY pin of ads0 
+  pinMode(ADS1115_RDY,INPUT_PULLUP);
+  // Interupt when RDY goes down
+  attachInterrupt(ADS1115_RDY, adcISR, FALLING);
+  /* The first sample is tricky. We need to start with adcReady true,
+     and read the register once. At the lowest sample rate (8SPS), we
+     need to wait 130 ms for the conversion to finish. All other rates
+     are faster.  */
+  adcReady = true;
+  ads0.startReadADC(channel[0],sampleRate);
+  delay(130);//
   
   // Initiliaze display
   tft.displayOn(true);
@@ -74,98 +166,104 @@ void setup () {
   tft.textTransparent(RA8875_WHITE);
   tft.textSetCursor(200, 1);
   tft.textWrite("TestADC.ino");
-
+  tft.textSetCursor(1, 1);
+  tft.textWrite("     Average Stddev");
 }
 
-
+/* buttonISR: runs when button is pressed */
+//void IRAM_ATTR adcISR()
+void  adcISR()
+ {
+   adcReady = true;
+ }
 
 void loop()
 {
-  uint16_t adc0, adc1, adc2, adc3, adc4, adc5, adc6, adc7; //Bit values of adc
-  char string[15]; //Buffer holding string for dtostrf
-
-  /* Read ADC1115 ports */
-  /* ADC 0 */
-  adc0=double(ads0.readADC_SingleEnded(0));
-  Serial.print("adc0: ");
-  Serial.println(adc0);
+  if (adcReady) {
+    sampletime=millis()-currenttime;
+    adcReady = false;
+    samplenum = samplenum +1;
+    adcValue=ads0.getADC();
+    if (samplenum % 5 == 0) { // slow down serial output
+      Serial.print(samplenum);
+      Serial.print(" channel: ");
+      Serial.print(channel[iChan]);
+      Serial.print( " Time: ");
+      Serial.print(sampletime);
+      Serial.print(" value: ");Serial.println(adcValue);
+    }
+    switch (channel[iChan]) {
+    case 0 : adc0[i]=adcValue;
+      break;
+    case 1 : adc1[i]=adcValue;
+      break;
+    case 2 : adc2[i]=adcValue;
+      break;
+    case 3 : adc3[i]=adcValue;
+      break;
+    }
+    iChan=iChan+1;//next channel
+    if (iChan > numChan-1) {
+      iChan=0;//fold over
+      i=i+1;  //fill arrays with next values
+    }
+    currenttime = millis();
+    ads0.startReadADC(channel[iChan],sampleRate);
+  }
   
-  adc1=double(ads0.readADC_SingleEnded(1));
-  Serial.print("adc1: ");
-  Serial.println(adc1);
+  if (i>=N) {
+    i=0; // Rest counter for next time
+    /* Read second ADC1115 ports. Does not have ALERT/RDY connected to
+       ESP32, so we just have to wait. 8SPS: 131 ms, 16SPS: 67 ms, 32SPS:
+       34ms, 64SPS 18 ms, 128SPS 10 ms, 250SPS 6 ms*/
+    int t_delay = sampletime+2;//add 2, just to be sure
+    for (int j = 0; j < N; j++)
+      { ads1.startReadADC(0,sampleRate);delay(t_delay);
+	adc4[j] = ads1.getADC();
+	ads1.startReadADC(1,sampleRate);delay(t_delay);
+	adc5[j] = ads1.getADC();
+	ads1.startReadADC(2,sampleRate);delay(t_delay);
+	adc6[j] = ads1.getADC();
+	ads1.startReadADC(3,sampleRate);delay(t_delay);
+	adc7[j] = ads1.getADC();
+      }
+
+    /* Calculate */
+    adc0r = analyze(adc0,N);
+    adc1r = analyze(adc1,N);
+    adc2r = analyze(adc2,N);
+    adc3r = analyze(adc3,N);
+    adc4r = analyze(adc4,N);
+    adc5r = analyze(adc5,N);
+    adc6r = analyze(adc6,N);
+    adc7r = analyze(adc7,N);
   
-  adc2=double(ads0.readADC_SingleEnded(2));
-  Serial.print("adc2: ");
-  Serial.println(adc2);
-  
-  adc3=double(ads0.readADC_SingleEnded(3));
-  Serial.print("adc3: ");
-  Serial.println(adc3);
+    /* Write to serial port */
+    Serial.print("time: ");
+    Serial.println(sampletime);
+    serialWrite(0,adc0r);
+    serialWrite(1,adc1r);
+    serialWrite(2,adc2r);
+    serialWrite(3,adc3r);
+    serialWrite(4,adc4r);
+    serialWrite(5,adc5r);
+    serialWrite(6,adc6r);
+    serialWrite(7,adc7r);			    
 
-  /* ADC 1 */
-  adc4=double(ads1.readADC_SingleEnded(0));
-  Serial.print("adc0: ");
-  Serial.println(adc4);
-  
-  adc5=double(ads1.readADC_SingleEnded(1));
-  Serial.print("adc1: ");
-  Serial.println(adc5);
-  
-  adc6=double(ads1.readADC_SingleEnded(2));
-  Serial.print("adc2: ");
-  Serial.println(adc6);
-  
-  adc7=double(ads1.readADC_SingleEnded(3));
-  Serial.print("adc3: ");
-  Serial.println(adc7);
-
-  /* Write to TFT screen */
-
-  //Write black square to erase
-  tft.fillRect(40,0,40,120, RA8875_BLUE);   
-  tft.textTransparent(RA8875_WHITE);
-  
-  //Write text
-  tft.textSetCursor(1, 1);
-  tft.textWrite("ADC0: ");
-  tft.textSetCursor(40, 1);
-  tft.textWrite(dtostrf(adc0,5,0,string),6);
-
-  tft.textSetCursor(1, 15);
-  tft.textWrite("ADC1: ");
-  tft.textSetCursor(40, 15);
-  tft.textWrite(dtostrf(adc1,5,0,string),6);
-
-  tft.textSetCursor(1, 30);
-  tft.textWrite("ADC2: ");
-  tft.textSetCursor(40, 30);
-  tft.textWrite(dtostrf(adc2,5,0,string),6);
-
-  tft.textSetCursor(1, 45);
-  tft.textWrite("ADC3: ");
-  tft.textSetCursor(40, 45);
-  tft.textWrite(dtostrf(adc3,5,0,string),6);
-
-  tft.textSetCursor(1, 60);
-  tft.textWrite("ADC4: ");
-  tft.textSetCursor(40, 60);
-  tft.textWrite(dtostrf(adc4,5,0,string),6);
-
-  tft.textSetCursor(1, 75);
-  tft.textWrite("ADC5: ");
-  tft.textSetCursor(40, 75);
-  tft.textWrite(dtostrf(adc5,5,0,string),6);
-
-  tft.textSetCursor(1, 90);
-  tft.textWrite("ADC6: ");
-  tft.textSetCursor(40, 90);
-  tft.textWrite(dtostrf(adc6,5,0,string),6);
-
-  tft.textSetCursor(1, 105);
-  tft.textWrite("ADC7: ");
-  tft.textSetCursor(40, 105);
-  tft.textWrite(dtostrf(adc7,5,0,string),6);
-
-
-  delay(200);
+    /* Write to TFT screen */
+    
+    //Write blue square to erase
+    tft.fillRect(40,20,100,120, RA8875_BLUE);   
+    tft.textTransparent(RA8875_WHITE);
+    //Write text
+    tftWrite(0,adc0r);
+    tftWrite(1,adc1r);
+    tftWrite(2,adc2r);
+    tftWrite(3,adc3r);
+    tftWrite(4,adc4r);
+    tftWrite(5,adc5r);
+    tftWrite(6,adc6r);
+    tftWrite(7,adc7r);			    
+  }
+  //delay(200);
 }
